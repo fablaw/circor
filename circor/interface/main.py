0287a75edb4ce3eb095bbd8a8adbe3c937a50271
Fabian Law <fbn_lw@hotmail.com>
2022-12-05 14:59:45 +0100

    Updated interface/preprocessing/ml_logics

--- circor/interface/main.py
+++ circor/interface/main.py
@@ -1,44 +1,114 @@
-from circor.preprocessing.preprocess_cnn import wav_to_2D_HD, wav_to_2D_compressed
-from circor.preprocessing.preprocess_rnn import wav_to_1D_padded, get_max_length
+from circor.preprocessing.preprocessing_csv import select_patients
+from circor.preprocessing.preprocess_denoise import download_reconstruct_upload, synchronise, reconstruct_signal, neigh_block, nb_beta
+from circor.preprocessing.preprocess_rnn import wav_to_1D
+from circor.ml_logics.data import rgba_data, rgba_new
+from circor.ml_logics.model import init_model, compile_model, train_model, evaluate_model
+from sklearn.model_selection import train_test_split
 import glob
 import time
+import numpy as np
+import os
+from google.cloud import storage
 
-def wav_to_2D(compressed_data=True):
+project=os.environ.get("PROJECT")
+bucket_name=os.environ.get("BUCKET_NAME")
+storage_client = storage.Client(project=project)
+bucket = storage_client.get_bucket(bucket_name)
 
-    ''' preprocess your recordings to tranform them into 2D arrays -for CNN purposes
-        2 options available : saving recordings in high quality or under uint8 format (latter is quicker)  '''
-    timestamp = time.strftime('%d_%H_%M')
+def preprocess():
 
-    if compressed_data:
+    ''' applying denoising functions on the original wave form, and output cleaned waveform  '''
+    print("\n⭐️ Use case: preprocess")
 
-        for wave_path in glob.glob('raw_data/training_data/*.wav'):
-            output = wav_to_2D_compressed(wave_path, hop_length=1024, save=True, timestamp=timestamp)
-    else:
+    tsv_blob = bucket.blob(f"tsv_raw")
+    csv_blob = bucket.blob(f"training_data.csv")
+    wav_blob = bucket.blob(f"audio_raw")
 
-        for wave_path in glob.glob('raw_data/training_data/*.wav'):
-            output = wav_to_2D_HD(wave_path, hop_length=1024, save=True, timestamp=timestamp)
+    wave_npy=wav_to_1D(wav_blob)
 
-    return
-
-def wav_to_1D():
-
-    ''' preprocess your recordings to tranform them into 1D arrays -for RNN purposes.
-        padding is done based on the maximum length available in the training set'''
-
-    max_length = get_max_length()
-    timestamp = time.strftime('%d_%H_%M')
-
-    for wave_path in glob.glob('raw_data/training_data/*.wav'):
-        output = wav_to_1D_padded(wave_path,
-                                  save=True,
-                                  wanted_length=max_length,
-                                  timestamp=timestamp)
-
-    return
+    df_final=select_patients(tsv_blob)
 
 
+
+
+
+
+
+
+
+
+
+    download_reconstruct_upload
+
+    return None
+
+
+def train():
+    """train a model on preprocessed data"""
+    print("\n⭐️ Use case: train")
+
+    X=rgba_data[0]
+    y=rgba_data[1]
+
+    X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.3, random_state=1)
+
+    #model params
+    learning_rate=0.0001
+    batch_size=32
+    patience=10
+
+    model=init_model(X_train)
+    model=compile_model(model, learning_rate=learning_rate)
+    model, history=train_model(model,
+                               X_train,
+                               y_train,
+                               batch_size=batch_size,
+                               patience=patience,
+                               validation_split=0.2,
+                               )
+
+    val_acc=np.max(history.history['val_accuracy'])
+    print(f"Validation set accuracy: {round(val_acc,2)}")
+    val_recall=np.max(history.history['val_recall'])
+    print(f"Validation set recall: {round(val_recall,2)}")
+
+    return (model, X_test, y_test)
+
+def evaluate():
+    '''evaluate on testing set'''
+    print("\n⭐️ Use case: evaluate")
+
+    metrics=evaluate_model(train()[0], #-> model
+                           train()[1],   #-> X_test
+                           train()[2],   #-> y_test
+                           batch_size=32
+                          )
+
+    print(f"Testing set accuracy: {round(metrics['accuracy'],2)}")
+    print(f"Testing set recall: {round(metrics['recall'],2)}")
+
+    return None
+
+def pred():
+    """
+    Evaluate the performance of the latest production model on new data
+    """
+
+    print("\n⭐️ Use case: predict")
+
+    X_processed=rgba_new()
+    model=train()[0]
+    y_pred=model.predict(X_processed)
+
+    print("\n✅ prediction done!")
+
+    if round(y_pred,2) >= 0.50:
+        return f"There is a high probability that murmurs exist"
+    return f"There is a high probability that murmurs do not exist"
 
 if __name__ == '__main__':
 
-    wav_to_2D(compressed_data=True)
-    wav_to_1D()
+    preprocess
+    train
+    evaluate
+    pred
--- circor/preprocessing/preprocess_rnn.py
+++ circor/preprocessing/preprocess_rnn.py
@@ -7,23 +7,11 @@ from google.cloud import storage
 from circor.parameters.params import BUCKET_NAME, PROJECT
 import os
 
-def get_max_length():
-
-    '''Find maximum audio length among all recordings in training_data
-    Output used to pad all data at the same length : the largest one'''
-
-    audio_length = list()
-
-    for file in glob.glob('raw_data/training_data/*.wav'):
-        sig, srate = librosa.load(file, sr = None)
-        audio_length.append(sig.shape[0])
-
-    max_length = max(audio_length)
-
-    return max_length
+storage_client = storage.Client(project=PROJECT)
+bucket = storage_client.get_bucket(BUCKET_NAME)
 
 
-def wav_to_1D_padded(wave_path, wanted_length= 90000, save=False,sr = None, timestamp = time.strftime('%d_%H_%M') ):
+def wav_to_1D(wave, save=False, sr = None, timestamp = time.strftime('%d_%H_%M') ):
 
     '''
     Converting a wav file to a padded ndarray and optional saving.
@@ -31,27 +19,19 @@ def wav_to_1D_padded(wave_path, wanted_length= 90000, save=False,sr = None, time
      pad_width = (O, N) allows to pad only on the right end of the sequence."""
 
     '''
-
+    for wave_path in wave:
     # wav to np.array
-    sig, srate = librosa.load(wave_path, sr = sr)
-    # add padding
-    if wanted_length > sig.shape[0]:
-        sig = np.pad(sig,pad_width = (0, wanted_length - sig.shape[0]), mode = 'constant', constant_values = -10)
+        sig, srate = librosa.load(wave_path, sr = sr)
 
+        if save:
+            new_path = f"{'/'.join(wave_path.split('.')[0].split('/')[:-1])}/test_numpy/{wave_path.split('.')[0].split('/')[-1]}.npy"
+            np.save(new_path, sig)
+            blob_path = wave_path.split('/')[-1].split('.')[0] #define name of processed wave file eg: '2530'
 
-    if save:
-        new_path = f"{'/'.join(wave_path.split('.')[0].split('/')[:-1])}/test_numpy/{wave_path.split('.')[0].split('/')[-1]}.npy"
-        np.save(new_path, sig)
-        blob_path = wave_path.split('/')[-1].split('.')[0] #define name of processed wave file eg: '2530'
-
-        storage_client = storage.Client(project=PROJECT)
-        bucket = storage_client.get_bucket(BUCKET_NAME)
 
         #locate file in dedicated folder named after timestamp
-        blob = bucket.blob(f"processed_data_1D/{timestamp}/{blob_path}.npy")
-        blob.upload_from_filename(new_path)
-        os.remove(wave_path)
+            blob = bucket.blob(f"raw_1D/{timestamp}/{blob_path}.npy")
+            blob.upload_from_filename(new_path)
+            os.remove(wave_path)
 
-
-
-    return sig
+        return sig
