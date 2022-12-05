4848208b3f03cc6c41056705dc11b1598e3eb0aa
Fabian Law <fbn_lw@hotmail.com>
2022-12-05 16:05:23 +0100

    save changes on prep_rnn

--- circor/preprocessing/preprocess_rnn.py
+++ circor/preprocessing/preprocess_rnn.py
@@ -7,11 +7,8 @@ from google.cloud import storage
 from circor.parameters.params import BUCKET_NAME, PROJECT
 import os
 
-storage_client = storage.Client(project=PROJECT)
-bucket = storage_client.get_bucket(BUCKET_NAME)
 
-
-def wav_to_1D(wave, save=False, sr = None, timestamp = time.strftime('%d_%H_%M') ):
+def wav_to_1D(wave, wanted_length= 90000, save=False,sr = None, timestamp = time.strftime('%d_%H_%M') ):
 
     '''
     Converting a wav file to a padded ndarray and optional saving.
@@ -19,15 +16,18 @@ def wav_to_1D(wave, save=False, sr = None, timestamp = time.strftime('%d_%H_%M')
      pad_width = (O, N) allows to pad only on the right end of the sequence."""
 
     '''
+
     for wave_path in wave:
-    # wav to np.array
         sig, srate = librosa.load(wave_path, sr = sr)
+
 
         if save:
             new_path = f"{'/'.join(wave_path.split('.')[0].split('/')[:-1])}/test_numpy/{wave_path.split('.')[0].split('/')[-1]}.npy"
             np.save(new_path, sig)
             blob_path = wave_path.split('/')[-1].split('.')[0] #define name of processed wave file eg: '2530'
 
+            storage_client = storage.Client(project=PROJECT)
+            bucket = storage_client.get_bucket(BUCKET_NAME)
 
         #locate file in dedicated folder named after timestamp
             blob = bucket.blob(f"raw_1D/{timestamp}/{blob_path}.npy")
