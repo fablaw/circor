e37732b3f7f4e85498d7d75c011b048f55abe0f6
Fabian Law <fbn_lw@hotmail.com>
2022-12-06 14:56:20 +0100

    Updated preprocessing

--- circor/preprocessing/preprocess_rnn.py
+++ circor/preprocessing/preprocess_rnn.py
@@ -4,11 +4,14 @@ import librosa.display
 import glob
 import time
 from google.cloud import storage
-from circor.parameters.params import BUCKET_NAME, PROJECT
 import os
+from preprocessing.wav_to_1D import download_bucket_objects
 
 
-def wav_to_1D(wave, wanted_length= 90000, save=False,sr = None, timestamp = time.strftime('%d_%H_%M') ):
+project=os.environ.get("PROJECT")
+bucket_name=os.environ.get("BUCKET_NAME")
+
+def wav_to_1D(wave = None, save=True ,sr = None, timestamp = time.strftime('%d_%H_%M') ):
 
     '''
     Converting a wav file to a padded ndarray and optional saving.
@@ -16,22 +19,22 @@ def wav_to_1D(wave, wanted_length= 90000, save=False,sr = None, timestamp = time
      pad_width = (O, N) allows to pad only on the right end of the sequence."""
 
     '''
+    #retrieving data from google cloud
+    local_folder=download_bucket_objects(bucket_name=bucket_name,
+                            blob_path= 'audio_raw',
+                            local_path = f'circor/raw_data',
+                            timestamp=timestamp)
+    print(local_folder)
+    #retrieving data saved locally
+    for wave_file in glob.glob(os.path.join(local_folder,'/{timestamp}/audio_raw/*.wav')):
+        sig, srate = librosa.load(wave_file, sr = sr)
 
-    for wave_path in wave:
-        sig, srate = librosa.load(wave_path, sr = sr)
+    if save:
+        new_folder='../processed_data/'
+        new_path = new_folder + wave_file.rsplit('.wav')[0].split('/')[-1] + '.npy'
+        np.save(new_path, sig)
 
+    return new_folder
 
-        if save:
-            new_path = f"{'/'.join(wave_path.split('.')[0].split('/')[:-1])}/test_numpy/{wave_path.split('.')[0].split('/')[-1]}.npy"
-            np.save(new_path, sig)
-            blob_path = wave_path.split('/')[-1].split('.')[0] #define name of processed wave file eg: '2530'
-
-            storage_client = storage.Client(project=PROJECT)
-            bucket = storage_client.get_bucket(BUCKET_NAME)
-
-        #locate file in dedicated folder named after timestamp
-            blob = bucket.blob(f"raw_1D/{timestamp}/{blob_path}.npy")
-            blob.upload_from_filename(new_path)
-            os.remove(wave_path)
-
-        return sig
+if __name__ == '__main__':
+    wav_to_1D()
