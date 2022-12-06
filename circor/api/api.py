9f689d08c9e310cacda23fe67137b1529d7ae5e8
Albin James <jamesalbin44@gmail.com>
2022-12-06 11:46:27 +0100

    Partial modification; model.joblib unclear

--- circor/api/api.py
+++ circor/api/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from circor.interface.main import wav_to_2D
 import librosa
-
+from circor.interface.main import preprocess
 
 app = FastAPI()
 #app.state.model = print(wav_to_2D(compressed_data=True))
@@ -17,9 +17,23 @@ app.add_middleware(
 )
 
 @app.get('/predict')
-def predict():
+def predict(patient_id: int = 2530) -> dict:
+
+
+    X_pred = preprocess(patient_id)
+
+    model = app.state.model
+    X_processed = preprocess(X_pred)
+    y_pred = model.predict(X_processed)
+
+    if round(y_pred,2) >= 0.45:
+        return f"There is a high probability that murmurs exist" #Heart conditions in general?
+    return f"There is a high probability that murmurs do not exist"
+
+
+
     # yet to to code, remaining question : are we going with a recording or an image directly?
-    return {'Should i go see the cardiologist ?': 'yes'}
+ #   return {'Should i go see the cardiologist ?': 'yes'}
 
 @app.get("/")
 def index():
