b75457cce881f76ab9307177277ddd963343e5ee
Anna Gatty <anna.gatty@edu.escp.eu>
2022-12-07 15:34:37 +0100

    update api, requirements and app

--- circor/api/api.py
+++ circor/api/api.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from circor.interface.main import wav_to_2D
-import librosa
-from circor.interface.main import preprocess
+from pydub import AudioSegment
+from circor.interface.main import pred
 
 app = FastAPI()
-#app.state.model = print(wav_to_2D(compressed_data=True))
 
 
 app.add_middleware(
@@ -16,24 +14,16 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.get('/predict')
-def predict(patient_id: int = 2530) -> dict:
+model_cnn = load_model()
 
 
-    X_pred = select_patients(patient_id)
+@app.post('/predict')
+async def process_audio(audio_file) -> float:
 
-    model = app.state.model
-    X_processed = preprocess(X_pred)
-    y_pred = model.predict(X_processed)
+    audio = AudioSegment.from_file(audio_file)
+    y_pred = pred(X_pred= audio, model=model_cnn)
 
-    if round(y_pred,2) >= 0.45:
-        return f"There is a high probability that murmurs exist" #Heart conditions in general?
-    return f"There is a high probability that murmurs do not exist"
-
-
-
-    # yet to to code, remaining question : are we going with a recording or an image directly?
- #   return {'Should i go see the cardiologist ?': 'yes'}
+    return {'probability to have a murmur is': y_pred}
 
 @app.get("/")
 def index():
