709262ae9e394b07b2409a686ee29e07ae0a15da
Fabian Law <fbn_lw@hotmail.com>
2022-12-08 17:38:43 +0100

    Updated app

--- circor/app/app.py
+++ circor/app/app.py
@@ -9,7 +9,7 @@ import os
 #use the full page
 st.set_page_config(layout="wide")
 
-st.sidebar.title('Welcome on CardioBot!🩺 \n  ')
+st.sidebar.title('Welcome to CardioBot!🩺 \n  ')
 st.sidebar.subheader('Your autonomous heartbeat checkup')
 
 st.sidebar.write('Dear Patient,')
@@ -19,9 +19,9 @@ st.sidebar.write("On this online platform \
 st.sidebar.markdown('---')
 st.sidebar.title('How does it work ?')
 st.sidebar.write(" 1) Cardio bot will first convert your heart recording into a spectrogram. \
-                This way, you will be able to see a visual representation of your heartbeat")
+                This way, you will be able to see a visual representation of your heartbeat.")
 st.sidebar.write(" 2) In a second step, Cardio bot will \
-                detect if your heart has a murmur or not")
+                detect if your heart has a murmur.")
 phonocardiogram_raw = st.sidebar.file_uploader(label="Upload your phonocardiogram below :",
                                            type='.wav')
 
@@ -114,11 +114,12 @@ if st.session_state.stage > 0:
                            '85343_TV': '62 %'}
 
         st.markdown("<h1 style='text-align: center; color: blue; font-size:300%;'>Yes, please</h1>", unsafe_allow_html=True)
-        st.markdown("<h1 style='text-align: left; font-size:100%;'>Dear patient,</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: left; font-size:150%;'>Dear patient,</h1>", unsafe_allow_html=True)
         col1, col2, col3 = st.columns((3,1,3))
-        col1.write("<h1 style='text-align: left; font-size:100%;'>Having analysed your heartbeat recording, \
+        col1.write("<h1 style='text-align: left; font-size:150%;'>Having analysed your heartbeat recording, \
                  we found that there are  chances that you have a murmur.</h1>", unsafe_allow_html=True)
-        col1.write("<h1 style='text-align: left; font-size:100%;'>We would like to recommend you to go see your cardiologist!</h1>", unsafe_allow_html=True)
+        col1.write("<h1 style='text-align: left; font-size:150%;'>We would like to recommend you to go see your cardiologist!</h1>", unsafe_allow_html=True)
         col2.write('')
-        col3.metric("Probability of having a murmur", f"{prediction_dict[phonocardiogram_raw.name[:-4]]}")
+        col3.write("<h1 style=' font-size:150%;'>Probability of having a murmur :</h1>", unsafe_allow_html=True)
+        col3.subheader(f"{prediction_dict[phonocardiogram_raw.name[:-4]]}")
         st.write("<h1 style='text-align: center; color: blue; font-size:200%;'>See you soon on CardioBot, take care!</h1>", unsafe_allow_html=True)
